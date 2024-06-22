907fbdcfc5d5683b747d33ac5f8ef4d4f20521e1
Roman31X <die.haa65@gmail.com>
2024-06-22 15:25:00 -0500

    Capa service con CRUD agregada con exito

--- src/main/java/com/estudiantes/service/EstudianteSetvicio.java
+++ src/main/java/com/estudiantes/service/EstudianteSetvicio.java
@@ -1,27 +1,37 @@
 package com.estudiantes.service;
 
 import com.estudiantes.model.Estudiante;
+import com.estudiantes.repository.EstudianteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service        // Para que Spring reconozca la dependencia
 public class EstudianteSetvicio implements IEstudianteServicio{
+
+    @Autowired  // nos permite inyectar dependencias necesarias
+    private EstudianteRepositorio estudianteRepositorio;
+
     @Override
     public List<Estudiante> listarEstudiantes() {
-        return null;
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
     }
 
     @Override
     public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
-        return null;
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
     }
 
     @Override
     public void guardarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.save(estudiante);
     }
 
     @Override
     public void eliminarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.delete(estudiante);
     }
 }
