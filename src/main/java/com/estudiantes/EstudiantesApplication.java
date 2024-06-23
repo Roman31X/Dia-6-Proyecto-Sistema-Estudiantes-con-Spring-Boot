6d7ec5bd8c34876659e46373d41d0b21c31b6499
Roman31X <die.haa65@gmail.com>
2024-06-23 15:00:00 -0500

    Logger funcional para mensajes en consola

--- src/main/java/com/estudiantes/EstudiantesApplication.java
+++ src/main/java/com/estudiantes/EstudiantesApplication.java
@@ -1,6 +1,8 @@
 package com.estudiantes;
 
 import com.estudiantes.service.EstudianteServicio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,12 +15,22 @@ public class EstudiantesApplication implements CommandLineRunner {
 	@Autowired
 	private EstudianteServicio estudianteServicio;
 
+	//Para enviar mensaje a nivel de la consola
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
+
 	public static void main(String[] args) {
+		logger.info("Iniciando la Aplicación con Spring....");
+
+		//Levanta la fabrica de Spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+
+		logger.info("Aplicación finalizada...");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		logger.info("Se esta ejecutando la aplicación...");
 	}
 }
